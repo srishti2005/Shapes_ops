56b256421bd33ad447bf3431d7788868ae673c8c
Srishti Tripathi <tripathisrishti.2010@gmail.com>
2025-03-06 23:16:56 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,1 +1,19 @@
 //Square.java
+
+//Rectangle.java
+
+import java.util.*;
+class Square extends Shape{
+	public Square(double side){
+		super(side,0,0,4); //super keyword refers to the shape constructor
+		
+	}
+	
+	public double calculateArea(){
+		return  dim_one * dim_one;	
+	}
+	public double  calculatePerimeter(){
+		return 4 * (dim_one) ;
+	}
+}
+
