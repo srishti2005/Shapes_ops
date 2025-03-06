d24223d8c6d5a751ca001f62b2fb82b2db513ecc
Srishti Tripathi <tripathisrishti.2010@gmail.com>
2025-03-06 22:38:06 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,17 @@
+//Rectangle.java
+
+import java.util.*;
+class Rectangle extends Shape{
+	public Rectangle(double length , double breadth){
+		super(length,breadth,0,4); //super keyword refers to the shape constructor
+		
+	}
+	
+	public double calculateArea(){
+		return  dim_one * dim_two;	
+	}
+	public double  calculatePerimeter(){
+		return 2 * (dim_one + dim_two) ;
+	}
+}
 
